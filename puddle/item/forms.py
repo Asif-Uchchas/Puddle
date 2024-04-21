d824a114ee3ad999a63dbc6ad26abee298b0747b
Asif-Uddin-Ahmed <asifuchchas123@gmail.com>
2024-04-21 17:05:33 +0600

    Updated the edit items functionalities

--- puddle/item/forms.py
+++ puddle/item/forms.py
@@ -2,13 +2,12 @@ from django import forms
 
 from .models import Item
 
-INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border bg-gray-200'
+INPUT_CLASSES = 'w-full py-4 px-6 rounded-xl border'
 
 class NewItemForm(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ('category', 'name', 'description','price', 'image',)
-        
+        fields = ('category', 'name', 'description', 'price', 'image',)
         widgets = {
             'category': forms.Select(attrs={
                 'class': INPUT_CLASSES
@@ -24,5 +23,24 @@ class NewItemForm(forms.ModelForm):
             }),
             'image': forms.FileInput(attrs={
                 'class': INPUT_CLASSES
+            })
+        }
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'image', 'is_sold')
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
             }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            })
         }
